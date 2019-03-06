d22c59f7f49e38d4954e2d8b5e91180fbcde58a0
Veniel Dorestave <veniel@noukod.com>
2019-03-06 20:03:57 +0000

    Automatically backed up by Learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,6 +6,8 @@ class CustomersController < ApplicationController
   end
 
   def upload
+        LeadsWorker.perform_async(params[:leads].path)
+    redirect_to customers_path
     CSV.foreach(params[:leads].path, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
